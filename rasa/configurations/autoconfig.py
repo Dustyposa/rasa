2f67d00a4d13a8534d507d01bcb1df799884ba51
Christina Koss <c.koss@rasa.com>
2020-05-25 18:27:52 +0200

    add debug message

--- rasa/configurations/autoconfig.py
+++ rasa/configurations/autoconfig.py
@@ -1,8 +1,11 @@
+import logging
+
 from typing import Text, Dict, Any, List
 
 from rasa.constants import CONFIG_AUTOCONFIGURABLE_KEYS
-
 from rasa.utils import io as io_utils
+
+logger = logging.getLogger(__name__)
 
 
 # In future iterations, this function and the function `create_config_for_keys` below
@@ -11,7 +14,6 @@ from rasa.utils import io as io_utils
 # domain: Domain
 # stories: StoryGraph
 def get_autoconfiguration(config):
-
     autoconfig_keys = CONFIG_AUTOCONFIGURABLE_KEYS
     missing_keys = [k for k in autoconfig_keys if k not in config]
 
@@ -20,8 +22,14 @@ def get_autoconfiguration(config):
     return config
 
 
-def create_config_for_keys(config: Dict[Text, Any], keys: List[Text],) -> None:
+def create_config_for_keys(config: Dict[Text, Any], keys: List[Text]) -> None:
     import pkg_resources
+
+    if keys:
+        logger.debug(
+            f"The provided configuration does not contain the key(s) {keys}. "
+            f"Running automatic configuration for them now."
+        )
 
     default_config_path = pkg_resources.resource_filename(
         __name__, "default_config.yml"
@@ -35,6 +43,7 @@ def create_config_for_keys(config: Dict[Text, Any], keys: List[Text],) -> None:
     for key in keys:
         config[key] = default_config[key]
         config["autoconfigured"].add(key)
+        # maybe add a debug output to print the result of the autoconfiguration?
 
 
 def dump_config(config: Dict[Text, Any], config_path: Text) -> None:
